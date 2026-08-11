133a0a0259b984a1de09e86d4b3663f2dbbb6e8b
Induvarman <induvarman8@gmail.com>
2026-08-11 21:02:01 +0530

    add WIN_COMBINATIONS, make_move method, check_status, draw?

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,53 @@
 require 'pry-byebug'
 
 class Board
-  attr_accessor :board
+  WIN_COMBINATION = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ].freeze
+
+  attr_reader :panel
 
   def initialize
-    @board = Array.new(9, '-')
+    @panel = Array.new(9, '-')
   end
 
   def print_board
-    board.each_with_index do |val, idx|
+    panel.each_with_index do |val, idx|
       print "#{val} "
       puts if ((idx + 1) % 3).zero?
     end
   end
-end
 
-board = Board.new
-board.print_board
+  def make_move(player_move, position)
+    @panel[position] = player_move
+
+    if check_status player_move
+      print_board
+      puts "Player #{player_move} Won!"
+      true
+    elsif draw?
+      print_board
+      puts 'DRAW!'
+    end
+  end
+
+  protected
+
+  def valid_move?(position)
+    panel[position] == '-' || false
+  end
+
+  private
+
+  def check_status(player_move)
+    WIN_COMBINATION.any? do |combo|
+      combo.all? { |idx| panel[idx] == player_move }
+    end
+  end
+
+  def draw?
+    panel.none?('-')
+  end
+end
