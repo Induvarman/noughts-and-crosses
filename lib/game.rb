93ffbe2b76a48652222555efe6eef9cbe0db4c16
Induvarman <induvarman8@gmail.com>
2026-08-18 19:42:09 +0530

    rework play_round, play_game, add declare winner

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ require_relative 'board'
 require_relative 'player'
  
 class Game
-  attr_reader :board, :player_x, :player_o
+  attr_reader :board, :player_x, :player_o, :round_count
 
   def initialize
     @board = Board.new
@@ -10,17 +10,37 @@ class Game
     @player_o = Player.new('o', :blue)
   end
 
+  def play_game
+    5.times do |round|
+      puts "ROUND #{round + 1}"
+      play_round
+      puts "\nPlayer #{player_x.sign} Score: #{player_x.wins}"
+      puts "Player #{player_o.sign} Score: #{player_o.wins}\n\n"
+    end
+    declare_winner
+  end
+
+  private
+
   def play_round
     loop do
       player_x.position board
-      break if board.check_status? player_x.sign
+      break if board.check_status? player_x
 
       player_o.position board
-      break if board.check_status? player_o.sign
+      break if board.check_status? player_o
+    end
+    board.empty_board
+  end
+
+  def declare_winner
+    if player_x.wins > player_o.wins
+      puts "Player #{player_x.sign} Won the Series"
+    elsif player_x.wins < player_o.wins
+      puts "Player #{player_o.sign} Won the series"
+    else
+      puts 'TIE'
     end
   end
 end
 
-game = Game.new
-
-game.play_round
